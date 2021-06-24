7e590217bcb5096434fc0e962b62d462e578f848
Taylor McKay <taylormckay@gmail.com>
2021-06-23 22:05:28 -0700

    Some basic data exploration

--- home-prices/train.py
+++ home-prices/train.py
@@ -8,7 +8,13 @@ def main():
         labels_reader = csv.DictReader(fh)
 
         for row in labels_reader:
-            print(row)
+            print(row.keys())
+            numeric = []
+            for k,v in row.items():
+                if v.isnumeric():
+                    numeric.append(k)
+            print('Numeric')
+            print(numeric)
             break
 
 if __name__ == '__main__':
