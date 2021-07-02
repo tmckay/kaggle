ea134c87efef3b0aa21fbb88e9b0e610f9d0d0f7
Taylor McKay <taylormckay@gmail.com>
2021-07-01 22:12:08 -0700

    Generate metrics for predictions

--- home-prices/train.py
+++ home-prices/train.py
@@ -1,13 +1,14 @@
 import csv
+import random
 
 import numpy as np
 from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
 
 
 def main():
     # Read data
     with open('train.csv') as fh:
-        #labels_reader = csv.reader(fh)
         labels_reader = csv.DictReader(fh)
 
         X_feats = []
@@ -22,15 +23,28 @@ def main():
                 int(row['YearRemodAdd']),
                 int(row['OverallQual'])
             ])
-            Y_labels.append(row['SalePrice'])
+            Y_labels.append(int(row['SalePrice']))
+
+        split = int(len(X_feats) * .5)
+        X_feats_train = X_feats[:split]
+        X_feats_test = X_feats[split:]
+
+        Y_labels_train = Y_labels[:split]
+        Y_labels_test = Y_labels[split:]
 
         regr = linear_model.LinearRegression()
-        regr.fit(np.array(X_feats), np.array(Y_labels).reshape(-1, 1))
+        regr.fit(np.array(X_feats_train), np.array(Y_labels_train).reshape(-1, 1))
 
-        pred_year = [[1995, 7500, 800, 800, 2006, 8]]
-        pred_sale_price = int(regr.predict(pred_year)[0][0])
-        print(f'For a home built in {pred_year[0]}, we predict a price of ${pred_sale_price:,}')
+        single_features = [[1995, 7500, 800, 800, 2006, 8]]
+        pred_sale_price = int(regr.predict(single_features)[0][0])
+        print(f'For a home with features {single_features[0]}, we predict a price of ${pred_sale_price:,}')
 
+        test_predictions = regr.predict(X_feats_test)
+        mse = mean_squared_error(test_predictions, Y_labels_test)
+        print(f'Mean squared error is {mse}')
+
+        mape = mean_absolute_percentage_error(test_predictions, Y_labels_test) * 100
+        print(f'Mean absolute percentage error {mape:.2f}%')
 
 if __name__ == '__main__':
     main()
