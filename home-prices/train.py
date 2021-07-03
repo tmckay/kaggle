8b48912512f7fa709ac0f9b04f31d80d187fccc0
Taylor McKay <taylormckay@gmail.com>
2021-07-02 18:28:49 -0700

    Test different combinations of features

--- home-prices/train.py
+++ home-prices/train.py
@@ -7,22 +7,32 @@ from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
 
 
 def main():
+
+    features = ('YearBuilt', 'LotArea', '1stFlrSF', '2ndFlrSF', 'YearRemodAdd', 'OverallQual', 'SalePrice')
+
     # Read data
     with open('train.csv') as fh:
         labels_reader = csv.DictReader(fh)
+        
+        all_features = []
+        for row in labels_reader:
+            all_features.append(
+                {feat: int(row[feat]) for feat in features}
+            )
+
+    for idx in range(1, len(features)): 
 
         X_feats = []
         Y_labels = []
 
-        for row in labels_reader:
-            X_feats.append([
-                int(row['YearBuilt']),
-                int(row['LotArea']),
-                int(row['1stFlrSF']),
-                int(row['2ndFlrSF']),
-                int(row['YearRemodAdd']),
-                int(row['OverallQual'])
-            ])
+        included_features = features[:idx]
+
+        print(included_features)
+
+        for row in all_features:
+            X_feats.append(
+                [int(row[feat]) for feat in included_features]
+            )
             Y_labels.append(int(row['SalePrice']))
 
         split = int(len(X_feats) * .5)
@@ -35,16 +45,13 @@ def main():
         regr = linear_model.LinearRegression()
         regr.fit(np.array(X_feats_train), np.array(Y_labels_train).reshape(-1, 1))
 
-        single_features = [[1995, 7500, 800, 800, 2006, 8]]
-        pred_sale_price = int(regr.predict(single_features)[0][0])
-        print(f'For a home with features {single_features[0]}, we predict a price of ${pred_sale_price:,}')
-
         test_predictions = regr.predict(X_feats_test)
         mse = mean_squared_error(test_predictions, Y_labels_test)
         print(f'Mean squared error is {mse}')
 
         mape = mean_absolute_percentage_error(test_predictions, Y_labels_test) * 100
         print(f'Mean absolute percentage error {mape:.2f}%')
+
 
 if __name__ == '__main__':
     main()
